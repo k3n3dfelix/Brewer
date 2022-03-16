8a6986283a43663255f9fa60751aafd701a0b0d8
kenedfelix <kened.felix@gmail.com>
2022-03-16 13:37:59 -0300

    Adicionado o Log4j2 ao projeto

--- src/main/java/com/algaworks/brewer/controller/CervejasController.java
+++ src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -2,6 +2,8 @@ package com.algaworks.brewer.controller;
 
 import javax.validation.Valid;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -14,9 +16,16 @@ import com.algaworks.brewer.model.Cerveja;
 @Controller
 public class CervejasController {
 	
+	private static final Logger logger = LoggerFactory.getLogger(CervejasController.class);
+	
 	@RequestMapping("/cervejas/novo")
 	public String novo(Cerveja cerveja){
-		
+		//logger.error("Aqui é um log de erro");
+		//logger.info("Aqui é um log de info");
+		//Exemplo de logo de erro com debug
+		if(logger.isDebugEnabled()){
+			logger.error("Aqui é um log de erro");
+		}
 		return "cerveja/CadastroCerveja";
 	}
 	
