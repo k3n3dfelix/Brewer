bd54ed1e4f6715feed29d22cc64bc00ace8ef97b
kenedfelix <kened.felix@gmail.com>
2022-04-22 10:58:39 -0300

    Criado extensão de dialeto thymeleaf

--- src/main/resources/static/layout/javascripts/brewer.js
+++ src/main/resources/static/layout/javascripts/brewer.js
@@ -1,3 +1,27 @@
+//Aplicando Design Patters JS
+var Brewer = Brewer || {};
+
+Brewer.maskMoney = (function(){
+	function MaskMoney(){
+		this.decimal = $('.js-decimal');
+		this.plain = $('.js-plain');
+	}
+	MaskMoney.prototype.enable = function(){
+		this.decimal.maskMoney({decimal:',', thousands:'.'});
+		this.plain.maskMoney({precision:0, thousands: '.'});
+	}
+	return MaskMoney;
+}());
+
+
+$(function(){
+	var maskMoney = new Brewer.maskMoney();
+	maskMoney.enable();
+	
+}) 
+
+
+/*
 $(function(){
 	var decimal = $('.js-decimal');
 	decimal.maskMoney({decimal:',', thousands:'.'});
@@ -5,4 +29,4 @@ $(function(){
 	var plain = $('.js-plain');
 	plain.maskMoney({precision:0, thousands: '.'});
 	
-})
+}) */
